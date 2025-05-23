3f26bab4177a2bd9e980feff9c38d3fd395554f8
Carlos Malki <132589278+carlosmalki@users.noreply.github.com>
2025-05-23 20:15:28 +0200

    Jag har skapat spel sidan och börjat designa den med symboler och färg

--- app/src/main/java/com/example/mathrush/GameActivity.java
+++ app/src/main/java/com/example/mathrush/GameActivity.java
@@ -12,29 +12,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Vi skapar puls på vår "PLAY"-knapp
-        Button playButton = findViewById(R.id.playButton);
+        // Vi skapar puls på vår "Tillbacka"/Back knapp som tar oss tillbacka till startsidan
+        Button backButton = findViewById(R.id.backButton);
         Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
-        playButton.startAnimation(pulse);
+        backButton.startAnimation(pulse);
 
-        // Vi går från MainActivity dvs startsidan till GameActivity dvs spelsidan genom att trycka på vår "playButton"
-        playButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        // Vi går från GameActivity dvs spelsidan tillbaka till MainActivity dvs startsidan genom att trycka på vår "backButton"
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(GameActivity.this, MainActivity.class);
             startActivity(intent);
         });
-
     }
+
 }
